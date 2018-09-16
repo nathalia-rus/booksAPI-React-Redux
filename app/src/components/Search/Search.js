ff9327ccfa6a992c2992a75e191f945204380b2a
Nathalia Rus <nathaliarus@live.fr>
2018-09-16 17:44:31 +0200

    got my own api key

--- app/src/components/Search/Search.js
+++ app/src/components/Search/Search.js
@@ -1,7 +1,18 @@
 // npm install immutable
  import { fromJS } from "immutable"
+ import fetch from "isomorphic-fetch"
+ // when u got one: import apiKey from "auth/apiKey"
+ // https://books.google.com/books?uid=117217888285977420980&hl=fr
+
+ // AIzaSyAPlIAbAw6BHJgtOtUJJdQmbsN4ADFlgq8
+ // my id = 117217888285977420980
+
+
+ // api key
+const apiKey = AIzaSyAPlIAbAw6BHJgtOtUJJdQmbsN4ADFlgq8;
 
 // CONSTANTS 
+
 // we need 2 constants : 1 to set the books, and 1 to set value of inpt box / query 
 // 1 = books you get from the API, 2 = whatever you type in to the input field 
 
@@ -9,6 +20,7 @@ export const SET_BOOKS = 'SET_BOOKS'
 export const SET_QUERY = 'SET_QUERY'
 
 // ACTIONS
+
 //  2 actions: 1 our set books
 export function setBooks (books) {
   return {
@@ -28,8 +40,44 @@ export function setQuery (query) {
 }
 
 // other action later on: GET_BOOKS, to handle our fetching. 
+// now:
 
-// Actions handlers 
+export const getBooks = (query) => {
+  return (dispatch, getState) => {
+    dispatch(setQuery(query))
+    // okay, now our fetch call, go get url :
+    // GET https://www.googleapis.com/books/v1/volumes?q={search terms}
+    // later: get an API key of your own, or the server might lock you out. 
+      fetch("https://www.googleapis.com/books/v1/volumes?q=" + query + '&key=' + apiKey) // if API key: + apiKey 
+      .then(response => {
+        // we need to check the status 
+        if (response.status >= 400) {
+          throw new Error('Bad response from the server')
+        }
+        return response.json()
+        // return response or not working lol, and in format json!
+      }).then(books =>  {
+      // when we got our books back, we need to set it in the state 
+      // need to dispatch setBooks, and gonna pass in that books
+      dispatch(setBooks(books.items))
+      // this is actually gonna contain the actual data from the server 
+      // gonna be an object,and it's gonna have the items = array of objects which have our books
+      }).catch(error => {
+        console.log(error)
+      })
+      // catches the error 
+
+  }
+}
+
+
+
+// gonna expose this getbooks fn to the entire app, and attach this fn into the intpu box
+// and bind it on change event, so everytile u type, sending info from the input box into this fn 
+// gonna make a fetch and do all this stuff
+// dispatch: dispatches actions in this file, in this case, the setbooks fn in here 
+
+// ACTIONS HANDLERS
 
 const ACTION_HANDLERS =  {
   [SET_BOOKS]: (state, {payload: books}) => {
@@ -43,8 +91,7 @@ const ACTION_HANDLERS =  {
     }
 }
 
-//
-// Reducer
+// REDUCER 
 
 const initialState = fromJS({});
 export default(state = initialState, action) => {
